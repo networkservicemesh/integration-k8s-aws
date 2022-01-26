b57d90c0853f6f4a7da9ff515071ba5a57f46cce
Artem Glazychev <artem.glazychev@xored.com>
2022-01-26 16:24:42 +0700

    Add coredns
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- scripts/aws-start.sh
+++ scripts/aws-start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 pushd scripts && \
-AWS_REGION=us-east-2 go run ./... Create && \
+AWS_REGION=us-east-2 go run ./... Create
+kubectl describe nodes
+kubectl get pods --all-namespaces -o wide
+kubectl delete deployment -n kube-system coredns
+kubectl apply -f coredns.yaml
 popd || exit 0
